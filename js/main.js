cd8e556faf92a07394b557fdf2d9ea158efb444d
milleseseau <leojimlavi@gmail.com>
2021-04-22 00:12:41 -0500

    equalsButton fix

--- js/main.js
+++ js/main.js
@@ -19,8 +19,132 @@ const initApp = () => {
                         ? newInput
                         : `${currentValueElem.value}${newInput}`;
             }
-        })
-    })
+        });
+    });
+
+    const opButtons = document.querySelectorAll('.operator');
+    opButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+
+            // equal sign showing
+            if(newNumberFlag) {
+                previousValueELem.textContent = "";
+                itemArray = [];
+            }
+
+            const newOperator = event.target.textContent;
+            const currentVal = currentValueElem.value;
+
+            // need number first
+            if(!itemArray.length && currentVal == 0) return;
+            
+            // begin new equation
+            if(!itemArray.length) {
+                itemArray.push(currentVal, newOperator);
+                previousValueELem.textContent = 
+                    `${currentVal}
+                    ${newOperator}`
+                    return newNumberFlag = true;
+            }
+
+            // complete equation
+            if(itemArray.length) {
+                itemArray.push(currentVal);
+
+                const equationObj = {
+                    num1: parseFloat(itemArray[0]),
+                    num2: parseFloat(currentVal),
+                    op:itemArray[1]
+                }
+
+                equationArray.push(equationObj);
+                const equationString = 
+                `
+                ${equationObj['num1']}
+                ${equationObj['op']}
+                ${equationObj['num2']}
+                `;
+
+                const newValue = calculate(equationString, currentValueElem);
+
+                previousValueELem.textContent = 
+                    `${newValue} ${newOperator}`;
+
+                // start new equation
+                itemArray = [newValue, newOperator];
+                newNumberFlag = true;
+                console.log(equationArray);
+            }
+        });
+    });
+
+    const equalsButton = document.querySelector('.equals');
+    equalsButton.addEventListener('click', () => {
+        const currentVal = currentValueElem.value;
+        let equationObj;
+
+        // pressing equals repeatedly
+        if(!itemArray.length && equationArray.length) {
+            const lastEquation = equationArray[equationArray.length -1];
+            equationObj = {
+                num1: parseFloat(currentVal),
+                num2: lastEquation.num2,
+                op: lastEquation.op
+            }
+        } else if (!itemArray.length) {
+            return currentVal;
+        } else {
+            itemArray.push(currentVal);
+            equationObj = {
+                num1: parseFloat(itemArray[0]),
+                num2: parseFloat(currentVal),
+                op: itemArray[1]
+            }
+        }
+
+        equationArray.push(equationObj);
+
+        const equationString = 
+            `${equationObj['num1']} ${equationObj['op']} ${equationObj['num2']}`;
+        
+        calculate(equationString, currentValueElem);
+
+        previousValueELem.textContent = `${equationString} =`;
+
+        newNumberFlag = true;
+        itemArray = [];
+        console.log(equationArray);
+    });
+
+    const clearButtons = document.querySelectorAll('.clear, .clearEntry');
+    clearButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            currentValueElem.value = 0;
+            if(event.target.classList.contains('clear')) {
+                previousValueELem.textContent = '';
+                itemArray = [];
+            }
+        });
+    });
+
+    const deleteButton = document.querySelector('.delete');
+    deleteButton.addEventListener('click', () => {
+        currentValueElem.value = currentValueElem.value.slice(0, -1);
+    });
+
+    const signChangeButton = document.querySelector('.signChange');
+    signChangeButton.addEventListener('click', () => {
+        currentValueElem.value = parseFloat(currentValueElem.value) * -1;
+    });
+
 }
 
 document.addEventListener("DOMContentLoaded", initApp);
+
+const calculate = (equation, currentValueElem) => {
+    const regex = /(^[*/=])|(\s)/g;
+    equation.replace(regex, '');
+    const divByZero = /(\/0)/.test(equation);
+    if(divByZero) return currentValueElem.value = 0;
+    return currentValueElem.value = eval(equation);
+}
